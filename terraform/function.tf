e4dc02dc2812d2d1bea751049ec0648011728a60
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-07-11 18:59:48 -0400

    adding privs
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- terraform/function.tf
+++ terraform/function.tf
@@ -48,3 +48,12 @@ resource "google_cloudfunctions_function" "function" {
         google_storage_bucket_object.zip
     ]
 }
+
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  project        = google_cloudfunctions_function.function.project
+  region         = google_cloudfunctions_function.function.region
+  cloud_function = google_cloudfunctions_function.function.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
+}
